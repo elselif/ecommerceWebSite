8bff3117b66b9fa6214b6c43dbfbc01074a40970
Elif <elffemre@gmail.com>
2022-07-16 17:36:34 +0300

    v.0.1.3

--- BlazorEcommorce/BlazorEcommorce/Server/Controllers/WeatherForecastController.cs
+++ BlazorEcommorce/BlazorEcommorce/Server/Controllers/WeatherForecastController.cs
@@ -19,7 +19,7 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet]
+    [HttpGet] //get oluşturmak için gerekli
     public IEnumerable<WeatherForecast> Get()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
